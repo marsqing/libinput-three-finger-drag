3f69fc1d68bb1a78f7171dc203d4e272bb99d3ba
Dale A. Jackson <JacksonWrath@gmail.com>
2022-02-14 00:04:17 -0800

    Change to single-pixel precision
    Signed-off-by: Dale A. Jackson <JacksonWrath@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ fn main() {
                     let y: f32 = parts[7].parse().unwrap();
                     xsum += x;
                     ysum += y;
-                    if xsum.abs() > 5.0 || ysum.abs() > 5.0 {
+                    if xsum.abs() > 1.0 || ysum.abs() > 1.0 {
                         xdo.move_mouse_relative(xsum as i32, ysum as i32).unwrap();
                         xsum = 0.0;
                         ysum = 0.0;
